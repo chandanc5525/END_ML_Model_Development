43529f9d929c8099e7118fea863cf1041a49920d
unknown <chaudhari.chandan22@gmail.com>
2025-10-02 16:05:54 +0530

    Setup.py Done

---
+++ setup.py
@@ -0,0 +1,32 @@
+ # https://pythonhosted.org/an_example_pypi_project/setuptools.html
+ 
+import os
+from setuptools import find_packages,setup
+from typing import List
+ 
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+     '''
+     This function will return List of Requirements
+     '''
+     requirements = []
+     with open(file_path) as file_obj:
+         requirements = file_obj.readlines()
+         requirements = [req.replace('\n',"") for req in requirements]
+         
+         if HYPEN_E_DOT in requirements:
+             requirements.remove(HYPEN_E_DOT)
+             
+     return requirements
+            
+setup(
+     name = 'mlproject',
+     version = '1.0.0',
+     author = 'chandan chaudhari',
+     author_email='chaudhari.chandan22@gmail.com',
+     description='MLOps Production Code',
+     packages= find_packages(),
+     install_requires = get_requirements('requirements.txt')
+     
+     
+)
